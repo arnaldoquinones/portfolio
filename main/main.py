3b7957babfd0a7252f319337e96359174db5fcc1
Arnaldo <arnaldopedroquinones@gmail.com>
2024-12-14 19:33:50 -0300

    redondeando boton

--- main/main.py
+++ main/main.py
@@ -1,22 +1,38 @@
-"""Pagina profesional de mis trabajos  Data Tech y resumen curricular."""
-
 import reflex as rx
-
+import asyncio
 from rxconfig import config
-from .modulos import sidebar_item
-# from .modulos TopBannerSignup
-# from .modulos top_banner_signup
-
-from .modulos import sidebar_bottom_profile # Importar la barra lateral
+from .modulos import sidebar_bottom_profile
 from .modulos import top_banner_gradient
 from .modulos import top_banner_signup
 
+# ---------------------------
+# -- EFECTO TYPE WRITTING. --
+# ---------------------------
 class State(rx.State):
-    """The app state."""
-    ...
+    """The main application state."""
+    landingpage_text: str = ""
+
+    async def landingpage_update(self):
+        """Simulación de efecto de texto con animación de tipo escritura."""
+        while True:
+            for service in ["Music", "Poems", "Art", "Pictures"]:
+                if self.landingpage_text != "Discover my ":
+                    self.landingpage_text = "Discover my"
+                for char in service:
+                    await asyncio.sleep(0.2)
+                    self.landingpage_text += char
+                    yield
+                self.landingpage_text += "."
+                yield
+                await asyncio.sleep(1)
+                for char in range(len(service) + 1):
+                    await asyncio.sleep(0.09)
+                    self.landingpage_text = self.landingpage_text[:-1]
+                    yield
 
 
 def index() -> rx.Component:
+    """Componente principal que renderiza la vista principal de la app."""
     return rx.box(
         rx.hstack(
             sidebar_bottom_profile(),
@@ -34,7 +50,8 @@ def index() -> rx.Component:
                     top_banner_gradient(),
                     top_banner_signup(),
                     rx.link(
-                        rx.button("Go to my GitHub!"),
+                        rx.button("Go to my GitHub!",
+                                  border_radius="20px"),
                         href="https://github.com/arnaldoquinones",
                         is_external=True,
                     ),
@@ -45,14 +62,11 @@ def index() -> rx.Component:
                 flex="1",
             ),
         ),
-        min_height="100vh",  # Asegura que el fondo cubra al menos la pantalla completa
+        min_height="100vh",
         width="100vw",
-        background="linear-gradient(to bottom, #000066, #000000)",  # Fondo degradado
-        overflow_y="auto",  # Permite desplazamiento vertical si es necesario
+        background="linear-gradient(to bottom, #000066, #000000)",
+        overflow_y="auto",
     )
-
-
-
 
 
 app = rx.App()
