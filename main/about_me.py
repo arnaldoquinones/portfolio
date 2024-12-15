8ab97e5f711f93a7ef78523da436f98bbdeea368
Arnaldo <arnaldopedroquinones@gmail.com>
2024-12-14 21:14:31 -0300

    updating_about_me

---
+++ main/about_me.py
@@ -0,0 +1,73 @@
+import reflex as rx
+import asyncio
+from rxconfig import config
+from .modulos import sidebar_bottom_profile
+from .modulos import top_banner_gradient
+from .modulos import top_banner_signup
+
+# ---------------------------
+# -- EFECTO TYPE WRITTING. --
+# ---------------------------
+class State(rx.State):
+    """The main application state."""
+    landingpage_text: str = ""
+
+    async def landingpage_update(self):
+        """Simulación de efecto de texto con animación de tipo escritura."""
+        while True:
+            for service in ["Music", "Poems", "Art", "Pictures"]:
+                if self.landingpage_text != "Discover my ":
+                    self.landingpage_text = "Discover my"
+                for char in service:
+                    await asyncio.sleep(0.2)
+                    self.landingpage_text += char
+                    yield
+                self.landingpage_text += "."
+                yield
+                await asyncio.sleep(1)
+                for char in range(len(service) + 1):
+                    await asyncio.sleep(0.09)
+                    self.landingpage_text = self.landingpage_text[:-1]
+                    yield
+
+
+def index() -> rx.Component:
+    """Componente principal que renderiza la vista principal de la app."""
+    return rx.box(
+        rx.hstack(
+            sidebar_bottom_profile(),
+            rx.container(
+                rx.color_mode.button(position="top-right"),
+                rx.vstack(
+                    rx.heading("Welcome to my Professional web page!", size="9"),
+                    rx.image(
+                        src="https://github.com/arnaldoquinones/portfolio/blob/master/assets/foto_perfil.png?raw=true",
+                        width="150px",
+                        height="auto",
+                        border_radius="50%",
+                        alt="Foto de perfil",
+                    ),
+                    top_banner_gradient(),
+                    top_banner_signup(),
+                    rx.link(
+                        rx.button("Go to my GitHub!",
+                                  border_radius="20px"),
+                        href="https://github.com/arnaldoquinones",
+                        is_external=True,
+                    ),
+                    spacing="5",
+                    justify="center",
+                ),
+                padding="1em",
+                flex="1",
+            ),
+        ),
+        min_height="100vh",
+        width="100vw",
+        background="linear-gradient(to bottom, #000066, #000000)",
+        overflow_y="auto",
+    )
+
+
+app = rx.App()
+app.add_page(index)  # Agregar la página principal
