39a45478c8da647de19218f8e980e119afd95cb0
Arnaldo <arnaldopedroquinones@gmail.com>
2024-12-15 13:56:56 -0300

    mejora en skills

--- main/skills.py
+++ main/skills.py
@@ -4,21 +4,28 @@ from .modulos import sidebar_bottom_profile
 
 
 def skills() -> rx.Component:
-    """Página About Me."""
+    """Página Skills."""
     return rx.box(
-        rx.center(
-            rx.vstack(
-                sidebar_bottom_profile(),
-                rx.heading("Skills Page", size="3xl", color="white"),
-                rx.text(
-                    "Let me show you my skills.",
-                    size="lg",
-                    color="gray.200",
-                    text_align="center",
+        rx.hstack(
+            sidebar_bottom_profile(),  # Barra lateral
+            rx.container(
+                rx.vstack(
+                    rx.heading("Skills Page", size="3xl", color="white"),
+                    rx.text(
+                        "Let me show you my skills.",
+                        size="lg",
+                        color="gray.200",
+                        text_align="center",
+                    ),
+                    spacing="5",
+                    justify="center",
                 ),
+                padding="1em",
+                flex="1",
             ),
-            padding="2em",
         ),
         min_height="100vh",
+        width="100vw",
         background="linear-gradient(to bottom, #002266, #001122)",
+        overflow_y="auto",
     )
