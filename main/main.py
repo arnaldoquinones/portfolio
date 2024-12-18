4c46b86767afe16fde598f3d2f422f3f685dbd51
Arnaldo <arnaldopedroquinones@gmail.com>
2024-12-18 17:01:30 -0300

    borrando notas

--- main/main.py
+++ main/main.py
@@ -11,13 +11,6 @@ class State(rx.State):
     pass
 
 
-
-# Con más de 24 años de experiencia en el Banco ICBC, he desempeñado roles tanto en el área administrativa como en el comercial, específicamente como oficial de negocios retail. Durante mi tiempo en administración, adquirí habilidades significativas en la preparación de informes utilizando SQL, contribuyendo así a la eficiencia operativa y la toma de decisiones informadas.
-
-# Me gradué como Licenciado en Administración de Empresas por la Universidad de Buenos Aires, donde consolidé mi comprensión de estrategias empresariales y gestión eficaz de recursos. Me caracterizo por ser una persona proactiva, orientada a resultados y con un alto nivel de empatía hacia mis compañeros de equipo.
-
-# Mi enfoque meticuloso y pensante me ha permitido enfrentar desafíos complejos con soluciones innovadoras, siempre buscando optimizar procesos y mejorar la experiencia del cliente. Estoy entusiasmado por explorar nuevas oportunidades donde pueda aplicar mi experiencia y contribuir al éxito de proyectos empresariales ambiciosos.
-
 def index() -> rx.Component:
     """Componente principal que renderiza la vista principal de la app."""
     return rx.box(
