f5b79f90658a81e30d1d7d148cf7504f8bc9e40a
Arnaldo <arnaldopedroquinones@gmail.com>
2024-12-18 12:32:01 -0300

    contact form

--- main/modulos.py
+++ main/modulos.py
@@ -433,8 +433,43 @@ class TopBannerSignup(rx.ComponentState):
             
         )
 
+# -- CONTACT FORM ---
+
+class FormState(rx.State):
+    form_data: dict = {}
+
+    @rx.event
+    def handle_submit(self, form_data: dict):
+        """Handle the form submit."""
+        self.form_data = form_data
+
+def form_example():
+    return rx.vstack(
+        rx.form(
+            rx.vstack(
+                rx.input(
+                    placeholder="First Name",
+                    name="first_name",
+                ),
+                rx.input(
+                    placeholder="Last Name",
+                    name="last_name",
+                ),
+                rx.button("Submit", type="submit"),
+            ),
+            on_submit=FormState.handle_submit,
+            reset_on_submit=True,
+        ),
+        rx.divider(),
+        rx.heading("Results"), 
+        rx.text(FormState.form_data.to_string()),
+    )
 
 top_banner_signup = TopBannerSignup.create
+
+
+
+
 
 def suma(a,b):
     c=a+b
